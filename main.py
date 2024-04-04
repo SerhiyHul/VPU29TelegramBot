2f5c8b913159c6d7c570a23c42cdef428a5096b2
Serhiy Hul <hulsehiy18@gmail.coml>
2024-04-04 15:45:07 +0300

    Добавлення кнопки share contact

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import logging
 
-from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 from dotenv import load_dotenv
 load_dotenv()
@@ -18,9 +18,10 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard = [
         [KeyboardButton('/hello')],
-        [KeyboardButton('Share my location', request_location = True)]
+        [KeyboardButton('Share my location', request_location=True)],
+        [KeyboardButton('Share my contact', request_contact=True)]
     ]
-    reply_text = ReplyKeyboardMarkup(keyboard, one_time_keyboard = True)
+    reply_text = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
     await update.message.reply_text(f"How can i help you {update.effective_user.first_name}?", reply_markup=reply_text)
 
 
@@ -48,6 +49,16 @@ async def location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     lat = update.message.location.latitude
     lon = update.message.location.longitude
     await update.message.reply_text(f"lat = {lat}, lon = {lon}")
+async def contact(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.message.contact.user_id
+    first_name = update.message.contact.first_name
+    last_name = update.message.contact.last_name
+    await update.message.reply_text(
+        f"""
+        user_id = {user_id}
+        first_name = {first_name}        
+        last_name = {last_name}
+        """, reply_markup=ReplyKeyboardRemove())
 
 
 app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
@@ -56,5 +67,7 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("hello", hello))
 location_handler = MessageHandler(filters.LOCATION, location)
 app.add_handler(location_handler)
+contact_handler = MessageHandler(filters.CONTACT, contact)
+app.add_handler(contact_handler)
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 app.run_polling()
